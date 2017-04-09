827b35aa80a964ca20cb604e04a016c2af802847
Jepsiko <Jepsiko@Gmail.com>
2017-04-09 10:57:03 +0200

    modif IASolvingCube access

--- src/IA/SolvingCube/IASolvingCube.java
+++ src/IA/SolvingCube/IASolvingCube.java
@@ -3,10 +3,10 @@ package IA.SolvingCube;
 import Cube.Cuboid;
 import IA.NeuralNetwork;
 
-public class IASolvingCube {
-    NeuralNetwork brain;
+class IASolvingCube {
+    private NeuralNetwork brain;
     
-    public IASolvingCube(Cuboid cube) {
+    IASolvingCube(Cuboid cube) {
         int[] topology = {cube.getAxisX()*cube.getAxisY()*cube.getAxisZ(), 20, 20, 3};
         brain = new NeuralNetwork(topology);
     }
